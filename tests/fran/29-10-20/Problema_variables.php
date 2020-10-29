9f4e723f08a54bb836e9eca26ec53413087fca0c
Your NameFranSlannn <you@example.comslannn@hotmail.com>
2020-10-29 20:41:50 +0100

    ejer

---
+++ tests/fran/29-10-20/Problema_variables.php
@@ -0,0 +1,30 @@
+<html>
+
+<head>
+    <title>Problema</title>
+</head>
+
+<body>
+  <?php
+  ~$edad = 33;
+  $peso = 90;
+  $Nombre = "Fran";
+  $Existe = true;
+  echo "Variable entera:";
+  echo $edad;
+  echo "---------- Se declara una variable de tipo integer.";
+  echo "<br>";
+  echo "Variable double:";
+  echo $peso;
+  echo "---------- Se declara una variable de tipo double.";
+  echo "<br>";
+  echo "Variable string:";
+  echo $Nombre;
+  echo "---------- Se declara una variable de tipo string.";
+  echo "<br>";
+  echo "Variable boolean:";
+  echo $Existe;
+  echo "---------- Se declara una variable de tipo boolean.";
+
+  ?>
+</body>
---
+++ tests/fran/29-10-20/variables.php
@@ -0,0 +1,31 @@
+<html>
+
+<head>
+  <title>Problema</title>
+</head>
+
+<body>
+  <?php
+  $dia = 24; //Se declara una variable de tipo integer.
+  $sueldo = 758.43; //Se declara una variable de tipo double.
+  $nombre = "juan"; //Se declara una variable de tipo string.
+  $exite = true; //Se declara una variable boolean.
+  echo "Variable entera:";
+  echo $dia;
+  echo "---------- Se declara una variable de tipo integer.";
+  echo "<br>";
+  echo "Variable double:";
+  echo $sueldo;
+  echo "---------- Se declara una variable de tipo double.";
+  echo "<br>";
+  echo "Variable string:";
+  echo $nombre;
+  echo "---------- Se declara una variable de tipo string.";
+  echo "<br>";
+  echo "Variable boolean:";
+  echo $exite;
+  echo "---------- Se declara una variable de tipo boolean.";
+  ?>
+</body>
+
+</html>
