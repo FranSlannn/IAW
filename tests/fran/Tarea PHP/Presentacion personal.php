124fe1860041e820fa300182fc6c0a704bf5b4a0
Your Name <you@example.com>
2020-11-01 13:49:45 +0100

    prueba1

---
+++ tests/fran/Tarea PHP/Presentacion personal.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Presentacion personal</title>
+</head>
+
+<body>
+  <?php
+  $Nombre = "Fran";
+  $Apellidos = "Ortega Gonzalez";
+  $Edad = 36;   
+  $Foto_url = <a title="foto" href="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSRwuzI-Qi0ufYWOfDu6VXssnUKVNIYD8HphA&usqp=CAU"><img src="Gato.jpg" alt="Gato" /></a>
+
+  ?>
+</body>
