ff83a9acae549f48c9296f97fe6ce64da55f556c
Your Name <you@example.com>
2020-11-01 16:10:21 +0100

    nuevo

---
+++ tests/fran/php/presentacion.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="sp">
+	
+<?php 
+include 'include/header.inc';
+ ?>
+   <style type="text/css">
+      body{
+            background-color:#f5f5f5; 
+      }
+      .form-inline {
+        max-width: 455px;
+        padding: 19px 29px 29px;
+        margin: 0 auto 20px;
+        background-color: #fff;
+        border: 1px solid #e5e5e5;
+        -webkit-border-radius: 5px;
+        }
+
+</style>
+
+<head>
+    <title>Presentacion personal</title>
+</head>
+
+<body>
+  <?php
+  $Nombre = "Francisco ";
+  $Apellidos = "Ortega Gonzalez";
+  $Edad = 36;
+  $Curso1 = Bachiller
+  
+  
+
+
+?>
+
+</body>
