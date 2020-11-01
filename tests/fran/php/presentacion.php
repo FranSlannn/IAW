634eff6a1f936b25eea4af504e35618d91baeb12
Your Name <you@example.com>
2020-11-01 19:05:36 +0100

    3

--- tests/fran/php/presentacion.php
+++ tests/fran/php/presentacion.php
@@ -5,16 +5,28 @@
  ?>
    <style type="text/css">
       body{
-            background-color:#f5f5f5; 
-      }
-      .form-inline {
-        max-width: 455px;
-        padding: 19px 29px 29px;
-        margin: 0 auto 20px;
-        background-color: #fff;
-        border: 1px solid #e5e5e5;
-        -webkit-border-radius: 5px;
+            background-color:#f5f5f5;   
+            max-width: 700px;
+            padding: 19px 29px 29px;
+            margin: 0 auto 20px;
+            background-color: #fff;
+            border: 1px solid #e5e5e5;
+            -webkit-border-radius: 5px;            
         }
+        h1{
+            color: #6c2eb9;
+            font-weight: normal;
+            font-size: 40px;
+            font-family: Arial;
+            text-transform: uppercase;  
+        }
+        h2 {
+            color: #3c1b66;
+            font-weight: normal;
+            font-size: 30px;
+            font-family: Arial;
+            text-transform: uppercase;
+            }
 
 </style>
 
@@ -29,14 +41,18 @@
   $Edad = 36;
   $Foto = 
   $Curso1 = "Bachiller";
-  $Curso2 = "Administracion de sistemas inforticos y redes";
+  $Curso2 = "Administracion de sistemas informaticos y redes";
   
   echo "<h1> Presentacion Personal </h1> <br>";
 
-  echo " Nombre: $Nombre <br>";
-  echo " Apellidos: $Apellidos <br>";
-  echo " Edad: $Edad <br>";
-  echo " Foto:  <br>";
+  echo " <b>Nombre:</b> $Nombre <br>";
+  echo " <b>Apellidos:</b> $Apellidos <br>";
+  echo " <b>Edad:</b> $Edad <br>";
+  echo " <b>Foto:</b> <br>";
+
+  echo "<h2>Estudios</h2><br>";
+  echo  "- $Curso1 <br>";
+  echo  "- $Curso2 <br>";
 
   
 
