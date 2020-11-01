436ecf9e9ae9323d06b837bdbb0b7de714f27abb
Your Name <you@example.com>
2020-11-01 15:59:55 +0100

    commit

--- tests/fran/Tarea PHP/Presentacion personal.php
+++ tests/fran/Tarea PHP/Presentacion personal.php
@@ -28,7 +28,7 @@ include 'include/header.inc';
   $Nombre = "Francisco ";
   $Apellidos = "Ortega Gonzalez";
   $Edad = 36;   
-  $Foto_url = <a title="foto" href="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSRwuzI-Qi0ufYWOfDu6VXssnUKVNIYD8HphA&usqp=CAU"><img src="Gato.jpg" alt="Gato" /></a>
+  $Foto_url = <a title="foto" href="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSRwuzI-Qi0ufYWOfDu6VXssnUKVNIYD8HphA&usqp=CAU"><img src="Gato.jpg" alt="Gato" /></a>;
   $Curso1 = Bachiller
   $Curso2 = Administracion de sistemas informacticos y redes
   
