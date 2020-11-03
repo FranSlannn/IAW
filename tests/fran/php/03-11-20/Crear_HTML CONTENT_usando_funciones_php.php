aa54629cea5e09b7b52fe974d471a44a12feda12
FranSlannn <you@example.com>
2020-11-03 21:04:13 +0100

    commit

---
+++ tests/fran/php/03-11-20/Crear_HTML CONTENT_usando_funciones_php.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+  <title>Funciones. Usando funciones php se deben generar el siguiente contenido</title>
+</head>
+
+<body>
+
+  <?php
+  
+  ?>
+
+</body>
+
+</html>
---
+++ tests/fran/php/03-11-20/Seleccion_aleatoria_de_asignatura.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+  <title>Array. Seleccion aleatoria de asignatura </title>
+</head>
+
+<body>
+
+  <?php
+  
+  ?>
+
+</body>
+
+</html>
