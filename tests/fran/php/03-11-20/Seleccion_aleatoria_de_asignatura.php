760cbae4fd3f5cef0f91259362c18438eae782c0
FranSlannn <you@example.com>
2020-11-04 20:06:23 +0100

    2

--- tests/fran/php/03-11-20/Seleccion_aleatoria_de_asignatura.php
+++ tests/fran/php/03-11-20/Seleccion_aleatoria_de_asignatura.php
@@ -8,11 +8,9 @@
 
 
 $asignaturas=array("iaw","seguridad","bases","sistemas","empresa");
-$random_keys=array_rand($asignaturas,3);
-
 
 $profesores=array("Fidel","Florindo","Javier","Marisol","Roberto");
-$random_keys=array_rand($profesores,3);
+
 
 echo "<p>El profesor <b>".getRandomArray($profesores). "</b> imparte la asignatura de <b>".getRandomArray($asignaturas)."</b>";
 
