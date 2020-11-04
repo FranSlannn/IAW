e4295499fa647a57960ba3ba300e4c56fb43cebd
FranSlannn <you@example.com>
2020-11-04 19:30:14 +0100

    1

--- tests/fran/php/03-11-20/Crear_HTML CONTENT_usando_funciones_php.php
+++ tests/fran/php/03-11-20/Crear_HTML CONTENT_usando_funciones_php.php
@@ -1,15 +1,34 @@
-<html>
 
-<head>
-  <title>Funciones. Usando funciones php se deben generar el siguiente contenido</title>
-</head>
+<?php
+// Funciones. Usando funciones php se deben generar el siguiente contenido
 
-<body>
+function openHtmlTag(){
+	echo "<html>";
+}
+function closeHtmlTag(){
+	echo "</html>";
+}
+function openTag($tag, $id, $class = ""){
+	echo "<".$tag." id=".$id." class=".$class.">";
+}
+function closeTag($tog){
+	echo "</".$tog.">";
+}
+function sayHello(){
+	echo "Hello";
+}
 
-  <?php
-  
-  ?>
-
-</body>
-
-</html>
+function sayWhatever($whatever){
+	echo "<b>".$whatever."</b>";
+}
+openHtmlTag();
+openTag("p", "id1");
+sayHello();
+closeTag("p");
+openTag("p", "id2");
+sayWhatever("Estoy usando una función");
+closeTag("p");
+openTag("p", "id3", "redcolor");
+sayWhatever("Estoy usando una función OTRA VEZ");
+closeTag("p");
+closeHtmlTag();
