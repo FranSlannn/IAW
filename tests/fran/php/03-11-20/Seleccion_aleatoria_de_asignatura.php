93370434267892660d6f516d793eb98eca091dfa
FranSlannn <you@example.com>
2020-11-04 19:53:09 +0100

    1

--- tests/fran/php/03-11-20/Seleccion_aleatoria_de_asignatura.php
+++ tests/fran/php/03-11-20/Seleccion_aleatoria_de_asignatura.php
@@ -1,15 +1,22 @@
-<html>
 
-<head>
-  <title>Array. Seleccion aleatoria de asignatura </title>
-</head>
 
-<body>
 
-  <?php
-  
-  ?>
 
-</body>
+<?php
 
-</html>
+//Array. Seleccion aleatoria de asignatura
+
+
+$asignaturas=array("iaw","seguridad","bases","sistemas","empresa");
+$random_keys=array_rand($asignaturas,3);
+echo $asignaturas[$random_keys[0]]."<br>";
+echo $asignaturas[$random_keys[1]]."<br>";
+echo $asignaturas[$random_keys[2]];
+
+$profesores=array("Fidel","Florindo","Javier","Marisol","Roberto");
+$random_keys=array_rand($profesores,3);
+echo $profesores[$random_keys[0]]."<br>";
+echo $profesores[$random_keys[1]]."<br>";
+echo $profesores[$random_keys[2]];
+
+?> 
