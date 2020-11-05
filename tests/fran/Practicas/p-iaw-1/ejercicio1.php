a32e4464ecb7938f0881cd2c8e00334a13ce7458
Your Name <you@example.com>
2020-11-05 20:42:26 +0100

    1

--- tests/fran/Practicas/p-iaw-1/ejercicio1.php
+++ tests/fran/Practicas/p-iaw-1/ejercicio1.php
@@ -4,6 +4,7 @@
     <title>Como poner HTML en PHP</title>
   </head>
   <body>
-    <h1><?php echo "This message is from server side." ?></h1>
+    <h1>
+    <?php echo "This message is from server side." ?></h1>
   </body>
 </html>
