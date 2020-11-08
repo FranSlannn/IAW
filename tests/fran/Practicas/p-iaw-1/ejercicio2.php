c7cc6b88d366c36395b975a35eeee546e9d71bbf
Your Name <you@example.com>
2020-11-08 14:19:59 +0100

    tabla

--- tests/fran/Practicas/p-iaw-1/ejercicio2.php
+++ tests/fran/Practicas/p-iaw-1/ejercicio2.php
@@ -7,4 +7,9 @@ Muestra cómo manejar variables y cómo usar bucles
 
     */
 
+    $n = 3;
+    print ("<P>La tabla de multiplicar del $n es:</P>");
+    for ($i=1; $i<=10; $i++)
+       echo $n." x ".$i." = " . $n*$i . "<BR>";
+
     ?>
