6bb581fb1a81cdc030a5e5335bce596233956985
Your Name <you@example.com>
2020-11-08 14:42:46 +0100

    fecha

--- tests/fran/Practicas/p-iaw-1/ejercicio3.php
+++ tests/fran/Practicas/p-iaw-1/ejercicio3.php
@@ -7,4 +7,16 @@ Muestra cómo usar comentarios, tablas y funciones (propias y
 de biblioteca). También cómo usar el manual de PHP
     */
 
-    ?>
+    $fecha  = date ("j/n/Y H:i");
+    print ("$fecha") .  "<BR>";
+    
+ /* Tabla */   
+
+    $dia = date ("j");
+    $mes = date ("n");
+    $anio = date ("Y");
+
+    Print ("Hoy es " . $dia . " del " . $mes . " del año " . $anio . "<BR>");
+
+
+?>
