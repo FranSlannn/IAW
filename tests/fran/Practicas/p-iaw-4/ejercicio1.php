615408ecdaafb89b3814affa4c215849e975e876
Fran <slannn@hotmail.com>
2020-11-11 15:33:03 +0100

    1

---
+++ tests/fran/Practicas/p-iaw-4/ejercicio1.php
@@ -0,0 +1,8 @@
+<?php
+
+/*Crea 2 variables $a y $b. Dar valores a numéricos a esas variables y
+ usa un If que compruebe que variable es más grande y lo imprima en un texto.
+    */
+
+    ?>
+
---
+++ tests/fran/Practicas/p-iaw-4/ejercicio2.php
@@ -0,0 +1,8 @@
+<?php
+
+/*Usa una función Php que pasadas 2 variables como argumento
+ te devuelva la más grande de ellas. La función se debe llamar getGratherValue()
+
+    */
+
+    ?>
