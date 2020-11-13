f0cd8a15cde696b7ad09ac5cac210a0259d54e3a
Fran <slannn@hotmail.com>
2020-11-13 18:19:19 +0100

    fin

--- tests/fran/Practicas/p-iaw-4/ejercicio1.php
+++ tests/fran/Practicas/p-iaw-4/ejercicio1.php
@@ -1,8 +1,23 @@
+<html>
+<body>
 <?php
 
-/*Crea 2 variables $a y $b. Dar valores a numéricos a esas variables y
+/*Crea 2 variables $a y $b. Dar valores numéricos a esas variables y
  usa un If que compruebe que variable es más grande y lo imprima en un texto.
     */
 
-    ?>
+    
+      $a = 17;
+      $b = 5;
+                    
+             if ($a > $b){
+               echo "$a es mas grande que $b";
+            }else {
+                     echo "$b es mas grande que $a";
+            };   
+                 
+
+ ?>
+</body>
+</html>
 
