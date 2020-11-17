6eee910acb46fffd5f8f7ecaba7b79f4b249e7b1
Fran <slannn@hotmail.com>
2020-11-17 21:02:00 +0100

    1

---
+++ tests/fran/Practicas/p-iaw-5/ejer.php
@@ -0,0 +1,32 @@
+
+ 
+    <?php
+
+/* 
+
+Crear un Script php que use FOR
+
+El script debe pintar 10 dirrecciones de email con terminaciones diferentes del 0 al 9.
+Por ejemplo.
+Dado mi email florindo.lopdel@educa.jcyl.es.
+
+El script php debe tener como respuesta lo siguiente.
+
+Mi correo es florindo.lopdel0@educa.jcyl.es.
+Mi correo es florindo.lopdel2@educa.jcyl.es.
+.....
+.....
+Mi correo es florindo.lopdel9@educa.jcyl.es.
+
+    */
+
+    $email = "florindo.lopde@educa.jcyl.es";
+
+for ($i=0; $i<10 ;$x++) {
+  str_replace("@","$i@", "$email" );
+}
+
+  echo "Mi correo es $email. <br>";
+
+?> 
+   
