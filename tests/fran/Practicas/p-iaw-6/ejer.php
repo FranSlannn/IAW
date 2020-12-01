cb4ca15f6c48fe13dce75c4b6c51b3e861bf7dd0
Fran <slannn@hotmail.com>
2020-12-01 19:32:23 +0100

    1

---
+++ tests/fran/Practicas/p-iaw-6/ejer.php
@@ -0,0 +1,31 @@
+
+ 
+    <?php
+
+/* 
+PHP básico - Array
+
+Crear un Script php que use un ARRAY.
+
+Crear un array con todas las provincias de Castilla y León.
+Imprimir Las ciudades en el html.
+Crea una función que se llame getProvinciasCYL().
+La funcion debe devolver un array con las provincias de Castilla y León
+crea una variable con el valor que devuelva esta función (array)
+Imprimir todas las ciudades en el html usando una lista ordenada de html
+
+Usa un html para la salida del script
+
+    */
+
+    $email = "florindo.lopde@educa.jcyl.es";
+
+for ($i=0; $i<10 ;$i++) {
+  echo str_replace("@",$i."@","Mi correo es ". $email."<br>");
+   
+}
+
+  
+
+?> 
+   
