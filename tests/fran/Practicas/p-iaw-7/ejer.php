cb77c4f2666ff5323948672d0aa5957a26b4b8aa
Fran <slannn@hotmail.com>
2020-12-01 20:11:58 +0100

    1

---
+++ tests/fran/Practicas/p-iaw-7/ejer.php
@@ -0,0 +1,52 @@
+
+ 
+    <?php
+
+/* 
+Crear un Script php que use un FORMULARIO.
+
+        El formulario debe tener dos imputs numéricos (input_a) (input_b)
+        El formulario debe tener un botn submit con el valor "Sumar"
+        El formulario debe tener un action que redirija a la misma página donde esté el form
+        Una vez enviado el formulario se debe validar que los dos valores que vienen de input_a e input_b no son vacíos
+        Una vez enviado y validado el formulario debe mostrar el siguiente texto.
+        "El valor de la suma de (valor de input_a) + (valor de input_b) es igual  a (resultado de la suma)"
+        Añadir los botones "Restar", "Multiplicar", "Dividir" y mostrar los resultados.
+        Dependiendo del botón que se pulse se debe hacer una operación u otra
+
+Usa un html para la salida del script
+
+    */
+
+    
+    ?> 
+
+
+
+<html>
+	<body>
+    <ol>
+      <?php
+      $provincias = [
+          "Avila",
+          "Burgos",
+          "Leon",
+          "Palencia",
+          "Salamanca",
+          "Segovia",
+          "Soria",
+          "Valladolid",
+          "Zamora"
+      ];
+          
+
+      
+      foreach( $provincias as $provincia) {    
+           
+            echo "<li>".$provincia."</li>";
+            
+        }
+        ?> 
+        </ol>
+    </body>
+  </html>
