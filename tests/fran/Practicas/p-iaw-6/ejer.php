e77a111d6992e90771992c6aa6bdb5cbbe76cae3
Fran <slannn@hotmail.com>
2020-12-01 20:10:45 +0100

    2

--- tests/fran/Practicas/p-iaw-6/ejer.php
+++ tests/fran/Practicas/p-iaw-6/ejer.php
@@ -7,10 +7,10 @@ PHP básico - Array
 
 Crear un Script php que use un ARRAY.
 
-Crear un array con todas las provincias de Castilla y León.
-Imprimir Las ciudades en el html.
-Crea una función que se llame getProvinciasCYL().
-La funcion debe devolver un array con las provincias de Castilla y León
+-Crear un array con todas las provincias de Castilla y León.
+      *Imprimir Las ciudades en el html.
+-Crea una función que se llame getProvinciasCYL().
+      *La funcion debe devolver un array con las provincias de Castilla y León
 crea una variable con el valor que devuelva esta función (array)
 Imprimir todas las ciudades en el html usando una lista ordenada de html
 
@@ -18,14 +18,35 @@ Usa un html para la salida del script
 
     */
 
-    $email = "florindo.lopde@educa.jcyl.es";
+    
+    ?> 
 
-for ($i=0; $i<10 ;$i++) {
-  echo str_replace("@",$i."@","Mi correo es ". $email."<br>");
-   
-}
 
-  
 
-?> 
-   
+<html>
+	<body>
+    <ol>
+      <?php
+      $provincias = [
+          "Avila",
+          "Burgos",
+          "Leon",
+          "Palencia",
+          "Salamanca",
+          "Segovia",
+          "Soria",
+          "Valladolid",
+          "Zamora"
+      ];
+          
+
+      
+      foreach( $provincias as $provincia) {    
+           
+            echo "<li>".$provincia."</li>";
+            
+        }
+        ?> 
+        </ol>
+    </body>
+  </html>
